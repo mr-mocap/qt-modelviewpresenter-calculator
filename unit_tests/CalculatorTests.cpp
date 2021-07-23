2eed6521e0c66d6d85f993b78e8e1e792490c1c5
David Harrison <16119203+mr-mocap@users.noreply.github.com>
2021-07-22 20:24:02 -0400

    Add basic Calculator tests

--- unit_tests/CalculatorTests.cpp
+++ unit_tests/CalculatorTests.cpp
@@ -7,55 +7,53 @@
 
 using namespace testing;
 
+class CalculatorTest : public testing::TestWithParam<TestEntry> {
+    // You can implement all the usual fixture class members here.
+    // To access the test parameter, call GetParam() from class
+    // TestWithParam<T>.
+};
+
 namespace
 {
+TestEntry TestValues[] = {
+                         {"1", "1"},
+                         {"2", "2"},
+                         {"23", "23"},
+                         {"234", "234"},
+                         {"1+", "1"},
+                         {"1+1", "1"},
+                         {"1+1=", "2"},
+                         {"10+1", "1"},
+                         {"10+1=", "11"},
+                         {"6+=", "12"},
+                         {"18/2=", "9"},
+                         {"5/1=", "5"},
+                         {"1/5=", "0"}, // Integer operations currently!
 
-TestEntry Tests[] = {
-    {"1", "1"},
-    {"2", "2"},
-    {"23", "23"},
-    {"234", "234"},
-    {"1+", "1"},
-    {"1+1", "1"},
-    {"1+1=", "2"},
-    {"10+1", "1"},
-    {"10+1=", "11"},
-    {"6+=", "12"},
-    {"18/2=", "9"},
-    {"5/1=", "5"},
-    {"1/5=", "0"}, // Integer operations currently!
+                         // Operator Precedence
+                         {"6+3*1=", "9"},
+                         {"6*3+1=", "19"},
+                         {"6*3/2=", "9"},
+                         {"1-1+1=", "1"}
+                         };
 
-    // Operator Precedence
-    {"6+3*1=", "9"},
-    {"6*3+1=", "19"},
-    {"6*3/2=", "9"},
-    {"1-1+1=", "1"}
-};
-#if 0
 void PressKeys(Calculator &calculator, const std::string &keypresses)
 {
     for (const char key : keypresses)
         calculator.pressKey(key);
 }
 
-void CheckDisplay(Calculator &calculator, const std::string &expected_display_value)
-{
-    assert(calculator.state().display == expected_display_value);
 }
 
-void RunTest(const TestEntry &test)
+TEST_P(CalculatorTest, BasicInputGivesExpectedResults)
 {
     Calculator calculator;
 
-    PressKeys(calculator, test.input_keys);
-    CheckDisplay(calculator, test.expected_display);
-}
-#endif
+    PressKeys(calculator, GetParam().input_keys);
+
+    EXPECT_THAT(calculator.state().display, Eq(GetParam().expected_display));
 }
 
-TEST(Calculator, BasicOperationsGiveExpectedResults)
-{
-    EXPECT_THAT(0, Eq(0));
-}
+INSTANTIATE_TEST_SUITE_P(BasicInputTest, CalculatorTest, testing::ValuesIn(TestValues));
 
 #endif // CALCULATORTESTS_H
